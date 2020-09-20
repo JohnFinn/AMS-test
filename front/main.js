2ba7c53fef02c630160e810784f587e19ecbd6f9
Jouni Sunnari <dz4tune@gmail.com>
2020-09-20 20:51:48 +0300

    implemented addition and deletion

--- front/main.js
+++ front/main.js
@@ -5,6 +5,20 @@ var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
+function onMapClick(e) {
+    if (confirm("add point at " + e.latlng)) {
+        axios.get(`/api/add_markers?name=${prompt('type name')}&x=${e.latlng.lat}&y=${e.latlng.lng}`).then(function(){});
+    }
+}
+
+function onMarkerClick(m) {
+    if (confirm("delete " + this.name)) {
+        axios.get(`/api/del_markers?name=${this.name}`).then(function(){});
+    }
+}
+
+mymap.on('click', onMapClick);
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -22,7 +36,11 @@ async function main() {
         }
         markers = {};
         for (var key in resp.data) {
-            markers[key] = L.marker(resp.data[key]).addTo(mymap);
+            let marker = L.marker(resp.data[key]);
+            marker.name = key;
+            marker.on('click', onMarkerClick);
+            markers[key] = marker;
+            marker.addTo(mymap);
         }
         await sleep(2000);
     }
