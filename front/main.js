87437de2eca4bf437c90f2203d716b683fec764e
Jouni Sunnari <dz4tune@gmail.com>
2020-09-20 20:22:46 +0300

    some functionality

--- front/main.js
+++ front/main.js
@@ -4,3 +4,28 @@ var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+markers = {
+
+}
+
+async function main() {
+    while (true) {
+        console.log("updating markers");
+        var resp = await axios.get('/api/get_markers');
+        for (var key in markers) {
+            mymap.removeLayer(markers[key]);
+        }
+        markers = {};
+        for (var key in resp.data) {
+            markers[key] = L.marker(resp.data[key]).addTo(mymap);
+        }
+        await sleep(2000);
+    }
+}
+
+main().then(function(){});
--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, send_from_directory, request
+import os
 
 app = Flask(__name__)
 
+markers = {
+    "a": (51.5, -0.09),
+    "b": (51.6, -0.09)
+}
 
-@app.route('/')
-def hello():
-    return 'Hello, World!, foo'
+@app.route('/api/get_markers')
+def get_markers():
+    return markers
+
+
+@app.route('/api/add_markers')
+def add_markers():
+    name = request.args.get('name')
+    x = float(request.args.get('x'))
+    y = float(request.args.get('y'))
+    markers[name] = (x,y)
+    return 'OK'
+
+
+@app.route('/api/del_markers')
+def del_markers():
+    name = request.args.get('name')
+    del markers[name]
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run()
